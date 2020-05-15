a7d25c826ac142d266b6cedd07d91e51d4b44f4a
frankiannelli <frank.iannelli@outlook.com>
2020-05-15 22:22:02 +1000

    main menu

--- src/graph/Main.java
+++ src/graph/Main.java
@@ -5,23 +5,67 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            Graph graph = loadFiles();
+            Scanner keyboard = new Scanner(System.in);
+           int loadingMenuOption = 0;
+            String[] loadingFiles = {"index.txt", "friends.txt"};
+            while (loadingMenuOption != 4) {
+                loadingMenuOption = showLoadingMenu();
+                switch (loadingMenuOption) {
+                    case 1:
+                        loadingMenuOption = 4;
+                        break;
+                    case 2:
+                        System.out.println("Enter the vertex file name");
+                        loadingFiles[0] = keyboard.nextLine();
+                        System.out.println("Enter the edge file name");
+                        loadingFiles[1] = keyboard.nextLine();
+                        loadingMenuOption = 4;
+                        break;
+                    case 3:
+                        System.out.println("Program ended - Goodbye");
+                        System.exit(0);
+                    default:
+                        System.out.println("Sorry, please enter valid Option");
+                }
+            }
+            Graph socialNetwork = loadFiles(loadingFiles[0], loadingFiles[1]);
             int option = 0;
-            while (option != 3) {
-                option = showMenu();
+            while (option != 7) {
+                option = showMainMenu();
                 switch (option) {
                     case 1:
                         System.out.println("Get ");
+
                         break;
                     case 2:
-                        System.out.println("message");
+                        System.out.println("Please enter the user name you would like to search for");
+//                        Scanner keyboard = new Scanner(System.in);
+                        String name = keyboard.nextLine();
+                        ArrayList<String> graphUsers = new ArrayList<String>();
+                        for(int i = 0; i < socialNetwork.size(); i++){
+                            graphUsers.add(String.valueOf(socialNetwork.getLabel(i)));
+                        }
+                        if(graphUsers.contains(name)){
+                            int[] neighbours = socialNetwork.neighbors(graphUsers.indexOf(name));
+                            Set<String> friends = new HashSet<String>();
+                            for(int i = 0; i < neighbours.length; i++){
+                                friends.add(String.valueOf(socialNetwork.getLabel(neighbours[i])));
+                                int[] neighboursNeighbours = socialNetwork.neighbors(neighbours[i]);
+                                for(int j = 0; j < neighboursNeighbours.length; j++){
+                                    friends.add(String.valueOf(socialNetwork.getLabel(neighboursNeighbours[i])));
+                                }
+                            }
+                            System.out.println(friends);
+                        } else {
+                            System.out.println("user not in the graph press enter to continue");
+                            String enter = keyboard.nextLine();
+                        }
                         break;
                     case 3:
                         System.out.println("message");
@@ -36,88 +80,70 @@ public class Main {
                         System.out.println("message");
                         break;
                     case 7:
-                        System.out.println("Goodbye3");
+                        System.out.println("Program ended - Goodbye");
                         System.exit(0);
                     default:
                         System.out.println("Sorry, please enter valid Option");
                 }
             }
         } catch (FileNotFoundException e) {
-            System.out.println("Can't find files, please check file path and try again");
+            System.err.println("Can't find files, please check file path and try again");
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
     }
-//Initialize the graph with the index file not the friends file
-    //
-    private static Graph loadFiles () throws IOException {
+
+    private static Graph loadFiles (String vertexFileName, String edgeFileName) throws IOException {
         try {
-            FileInputStream friends = new FileInputStream("friends.txt");
-            Scanner friendScanner = new Scanner(friends);
-            int linesInFriendsFile = 0;
-            int numberOfEdges = Integer.parseInt(friendScanner.nextLine());
-            linesInFriendsFile++;
-//            if (friendScanner.hasNextLine()) {
-//                linesInFriendsFile++;
-//                friendScanner.nextLine();
-//            }
-            Graph socialNetwork = new SocialNetwork(numberOfEdges);
-            System.out.println(numberOfEdges);
-            while (friendScanner.hasNextLine()) {
-                linesInFriendsFile++;
-                String edgeData = friendScanner.nextLine();
-                String[] edges = edgeData.split(" ");
-                int[] ary = new int[edges.length];
-                int i = 0;
-                for (String edge : edges)
-                    ary[i++] = Integer.parseInt(edge);
-
-//                System.out.println(Arrays.toString(ary));
-//                socialNetwork.addEdge(ary[0], ary[1]);
-                socialNetwork.addEdge(0, 1);
-                socialNetwork.addEdge(0, 2);
-                socialNetwork.addEdge(0, 3);
-                socialNetwork.addEdge(0, 4);
-                socialNetwork.addEdge(1, 4);
-
-            }
-
-            if(linesInFriendsFile -1 != numberOfEdges) {
-                throw new IOException("number edges in file is wrong");
-            }
-            friendScanner.close();
-            FileInputStream index = new FileInputStream("index.txt");
-            Scanner indexScanner = new Scanner(index);
-            int linesInIndexFile = 1;
-            int numberOfPeople = Integer.parseInt(indexScanner.nextLine());
-            if (indexScanner.hasNextLine()) {
-                linesInIndexFile++;
-                indexScanner.nextLine();
-            }
-            while (indexScanner.hasNextLine()) {
-                linesInIndexFile++;
-                String peopleData = indexScanner.nextLine();
+            FileInputStream vertexStream = new FileInputStream(vertexFileName);
+            Scanner vertexScanner = new Scanner(vertexStream);
+            int linesInVertexFile = 0;
+            int numberOfVertices = Integer.parseInt(vertexScanner.nextLine());
+            linesInVertexFile++;
+            Graph socialNetwork = new SocialNetwork(numberOfVertices);
+            while (vertexScanner.hasNextLine()) {
+                linesInVertexFile++;
+                String peopleData = vertexScanner.nextLine();
                 String[] people = peopleData.split(" ");
                 String[] ary = new String[people.length];
                 int i = 0;
                 for (String person : people)
                     ary[i++] = person;
-//                socialNetwork.setLabel(Integer.parseInt(ary[0]), ary[1]);
-                socialNetwork.setLabel(0, "frank");
+                socialNetwork.setLabel(Integer.parseInt(ary[0]), ary[1]);
             }
-            if(linesInIndexFile != numberOfPeople) {
-                throw new IOException("number people in file is wrong");
+            if(linesInVertexFile -1 != numberOfVertices) {
+                throw new IOException("number people in file is wrong please correct this error and run again");
             }
-            indexScanner.close();
+            vertexScanner.close();
+            FileInputStream edgeStream = new FileInputStream(edgeFileName);
+            Scanner edgeScanner = new Scanner(edgeStream);
+            int linesInEdgeFile = 0;
+            int numberOfEdges = Integer.parseInt(edgeScanner.nextLine());
+            linesInEdgeFile++;
+            while (edgeScanner.hasNextLine()) {
+                linesInEdgeFile++;
+                String edgeData = edgeScanner.nextLine();
+                String[] edges = edgeData.split(" ");
+                int[] ary = new int[edges.length];
+                int i = 0;
+                for (String edge : edges)
+                    ary[i++] = Integer.parseInt(edge);
+                socialNetwork.addEdge(ary[0], ary[1]);
+            }
+            if(linesInEdgeFile -1 != numberOfEdges) {
+                throw new IOException("number edges in file is wrong please correct this error and run again");
+            }
+            edgeScanner.close();
+
             return socialNetwork;
         } catch (FileNotFoundException e) {
-            throw new IOException("Files not found");
+            throw new IOException("1 or more files not found,\nPlease check file path and try again");
         } catch (IOException e) {
             throw e;
         }
     }
 
-    private static int showMenu() {
+    private static int showMainMenu() {
         int option = 0;
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Main Menu:");
@@ -129,6 +155,23 @@ public class Main {
         System.out.println("5.Delete user and relationships");
         System.out.println("6.Print user list sorted by popularity");
         System.out.println("7.Quit");
+        System.out.println("--------------");
+        System.out.println("Enter your choice:");
+        option = keyboard.nextInt();
+
+        return option;
+    }
+
+    private static int showLoadingMenu() {
+        int option = 0;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("SOCIAL NETWORK PROGRAM:");
+        System.out.println("--------------");
+        System.out.println("Welcome to the social network program");
+        System.out.println("--------------");
+        System.out.println("1.Start with default network");
+        System.out.println("2.Load a network from different files");
+        System.out.println("3.Quit");
         System.out.println("--------------");
         System.out.println("Enter your choice:");
         option = keyboard.nextInt();
---
+++ src/graph/Readme.md
@@ -0,0 +1,7 @@
+## Assumptions
+
+that 2 menu 2 the user does not want to see the relationship of the original user linking back to themselves eg Hulk knows Hulk
+
+## Things to fix
+
+the file validation logic is not right because if the graph is intastaited with the wrng number of nodes then it fails
